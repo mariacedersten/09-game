73e132ec87711433c8d5fe261c00a6c2ee1a3e5d
mariacedersten <s2401554@edu.bc.fi>
2025-05-11 21:17:26 +0300

    Tehty

---
+++ game/Program.cs
@@ -0,0 +1,36 @@
+﻿using Raylib_cs;
+
+namespace game;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+        Raylib.SetTargetFPS(30);
+        float textY = 0.0f;
+
+
+        while (Raylib.WindowShouldClose() == false)
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Red);
+
+            Raylib.DrawText("Merry Christmas!", 200, (int)textY, 50, Color.White);
+            Raylib.DrawCircle(50, 50, 20, Color.White);
+            Raylib.DrawCircle(50, 95, 30, Color.White);
+            Raylib.DrawCircle(50, 155, 40, Color.White);
+
+            Raylib.EndDrawing();
+
+            textY += 60.0f * Raylib.GetFrameTime();
+            if (textY > Raylib.GetScreenHeight())
+            {
+                textY = 0;
+            }
+
+        }
+
+        Raylib.CloseWindow();
+    }
+}
